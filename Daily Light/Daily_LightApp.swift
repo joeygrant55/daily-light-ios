d9beb8094c665535957a8e6d8c4fb9171edefe16
joeygrant55 <48562179+joeygrant55@users.noreply.github.com>
2025-04-15 21:43:15 -0400

    Initial Commit

---
+++ Daily Light/Daily_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_LightApp.swift
+//  Daily Light
+//
+//  Created by Joseph Grant on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Daily_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Daily LightTests/Daily_LightTests.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_LightTests.swift
+//  Daily LightTests
+//
+//  Created by Joseph Grant on 4/15/25.
+//
+
+import Testing
+@testable import Daily_Light
+
+struct Daily_LightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
